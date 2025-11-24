2c99a4a9396832172d9abc035e447b9b8d101fe2
onlie424 <onlienoel@gmail.com>
2025-11-24 15:26:36 +0000

    database and backend set up

---
+++ src/test/java/com/example/finalprojectb/FinalProjectBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalprojectb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
